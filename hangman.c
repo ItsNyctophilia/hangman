d44d543c79f56361ec8de7b6850e2a92b3115cfb
James Viner Esq <jrviner2000@gmail.com>
2022-11-03 21:08:32 -0400

    Add function to get linecount of file

--- hangman.c
+++ hangman.c
@@ -4,10 +4,13 @@
 #include <string.h>
 
 enum return_codes {
-	SUCCESS = 1
+	SUCCESS = 0,
+	FILE_ERROR = 1
 };
 
 char *generate_default_word_path(void);
+void select_word(char *word_path);
+int get_linecount(FILE * word_file);
 
 int main(int argc, char *argv[])
 // TODO: Everything
@@ -22,7 +25,7 @@ int main(int argc, char *argv[])
 		//words_file = argv[1];
 	} else {
 		word_path = generate_default_word_path();
-		// TODO: generate word
+		select_word(word_path);
 		free(word_path);
 	}
 
@@ -38,4 +41,57 @@ char *generate_default_word_path(void)
 	char *word_path = (char *)malloc(path_length);
 	snprintf(word_path, path_length, "%s/.words", getenv("HOME"));
 	return (word_path);
+}
+
+void select_word(char *word_path)
+{
+
+	// Sourced from Liam Echlin in get_random_lyrics.c
+	FILE *word_file;
+	if ((word_file = fopen(word_path, "r")) == NULL) {
+		perror("Could not open words file");
+		exit(FILE_ERROR);
+	}
+	int line_count = get_linecount(word_file);
+	//char *valid word = "words";
+	for (int i = 0; i < line_count; ++i) {
+
+	}
+
+	printf("%d <-- lines", line_count);
+}
+
+int get_linecount(FILE * word_file)
+// Gets count of how many lines of content there are 
+// for given file, ignoring duplicate newlines
+// and accounting for an absent newline before EOF.
+// Returns number of lines with content, valid or invalid.
+{
+	int line_count = 0;
+	char consumer = fgetc(word_file);
+	if (consumer == EOF) {
+		// Case: File is empty
+		return (line_count);
+	}
+	for (;;) {
+		// Case: File has content
+		if (consumer == '\n') {
+			++line_count;
+			if ((consumer = fgetc(word_file)) == EOF) {
+				break;
+			} else if (consumer == '\n') {
+				while ((consumer = fgetc(word_file)) == '\n') {
+					;
+				}
+				if (consumer == EOF) {
+					break;
+				}
+			}
+		} else if (consumer == EOF) {
+			++line_count;
+			break;
+		}
+		consumer = fgetc(word_file);
+	}
+	return (line_count);
 }
