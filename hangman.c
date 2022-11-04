4e42935fe76a134aae46e96ecc75d9a6e9601e0b
James Viner Esq <jrviner2000@gmail.com>
2022-11-04 01:16:26 -0400

    Add tokenization of input file strings functionality

--- hangman.c
+++ hangman.c
@@ -8,6 +8,10 @@ enum return_codes {
 	FILE_ERROR = 1
 };
 
+enum game_defaults {
+	MAX_WORD_LEN = 64
+};
+
 char *generate_default_word_path(void);
 void select_word(char *word_path);
 int get_linecount(FILE * word_file);
@@ -38,7 +42,7 @@ char *generate_default_word_path(void)
 {
 	size_t path_length = (strlen(getenv("HOME")) + 8);	// Length of path required for 
 	// $HOME + "/.words" + NULL byte
-	char *word_path = (char *)malloc(path_length);
+	char *word_path = (char *)malloc(sizeof(char) * path_length);
 	snprintf(word_path, path_length, "%s/.words", getenv("HOME"));
 	return (word_path);
 }
@@ -47,18 +51,45 @@ void select_word(char *word_path)
 {
 
 	// Sourced from Liam Echlin in get_random_lyrics.c
-	FILE *word_file;
-	if ((word_file = fopen(word_path, "r")) == NULL) {
+	FILE *word_file = fopen(word_path, "r");
+	if (!word_file) {
 		perror("Could not open words file");
 		exit(FILE_ERROR);
 	}
-	int line_count = get_linecount(word_file);
-	//char *valid word = "words";
-	for (int i = 0; i < line_count; ++i) {
+	size_t line_count = get_linecount(word_file);
 
+	char *string_list = (char *)calloc(line_count, sizeof(char) * MAX_WORD_LEN);	// 34 is max word length as per spec, rounded up to next power of 2 is 64
+	char line_buffer[MAX_WORD_LEN + 2];	// Size of max word length + \n | \0
+	for (size_t i = 0; i < line_count; ++i) {
+		fgets(line_buffer, (MAX_WORD_LEN + 2), word_file);
+		if (line_buffer[0] == '\n') {
+			--i;
+			continue;
+		} else if (line_buffer[strlen(line_buffer) - 1] != '\n'
+			   && !feof(word_file)) {
+			// Case: Line too long.
+			puts("here");
+			char consumer = '\0';
+			while (consumer != '\n' && consumer != EOF) {
+				consumer = getc(word_file);
+			}
+			--line_count;
+			--i;
+			continue;
+		}
+		strncpy(string_list + strlen(string_list), line_buffer,
+			strlen(line_buffer));
+	}
+	printf("String Listing - %s", string_list);
+	char **words =
+	    (char **)calloc(line_count, sizeof(char *) * MAX_WORD_LEN);
+	words[0] = strtok(string_list, "\n");
+
+	for (size_t i = 1; i < line_count; ++i) {
+		words[i] = strtok(NULL, "\n");
+		printf("Tokenized: %s\n", words[i]);
 	}
 
-	printf("%d <-- lines", line_count);
 }
 
 int get_linecount(FILE * word_file)
@@ -67,7 +98,7 @@ int get_linecount(FILE * word_file)
 // and accounting for an absent newline before EOF.
 // Returns number of lines with content, valid or invalid.
 {
-	int line_count = 0;
+	size_t line_count = 0;
 	char consumer = fgetc(word_file);
 	if (consumer == EOF) {
 		// Case: File is empty
@@ -93,5 +124,7 @@ int get_linecount(FILE * word_file)
 		}
 		consumer = fgetc(word_file);
 	}
+	// Set pointer to start of file
+	fseek(word_file, 0, SEEK_SET);
 	return (line_count);
 }
